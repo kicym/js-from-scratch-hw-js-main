5104579d337cdef60a1a4016b0caf254404a8239
kicym <kicym.111@gmail.com>
2025-06-26 19:38:13 +0300

    first task

---
+++ lesson-01/01.js
@@ -0,0 +1,12 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+let step = 1;
+
+while (step <= 20) {
+    sum = sum + step;
+    step++;
+}
+console.log(sum);
