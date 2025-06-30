28843e1db14c316e099ecee5a947d8022371c38f
kicym <kicym.111@gmail.com>
2025-06-30 20:18:45 +0300

    lesson-two-task-3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,20 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
+const score = 7 // тестовое значение, можно изменять
 let grade
 // your code
+
+if (score <= 49 && score >= 0) {
+    grade = "F"
+} else if (score <= 69) {
+    grade = "D"
+} else if (score <= 79) {
+    grade = "C"
+} else if (score <= 89) {
+    grade = "B"
+} else if (score <=100) {
+    grade = "A"
+}
+console.log(grade)
+
