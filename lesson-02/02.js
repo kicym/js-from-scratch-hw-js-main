e27ee43901d317b3f26ac17a6ef6b2cab9a0022e
kicym <kicym.111@gmail.com>
2025-06-30 19:51:13 +0300

    lesson-two-task-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,20 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6 + 1) // your code
+let dice2 = Math.floor(Math.random() * 6 + 1) // your code
+let isWinningDouble = false // your code
 
-/*
+
+if (dice1  === dice2 && dice1 > 3 && dice2 > 3) {
+    isWinningDouble = true
+}
+
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+    console.log('Выигрышный дубль!')
 } else {
-  console.log('Не выигрышный дубль.')
+    console.log('Не выигрышный дубль.')
 }
-*/
