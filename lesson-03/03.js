1a45bd92d4d2a4861228359fc48f2211f03a44ad
kicym <kicym.111@gmail.com>
2025-07-10 16:11:13 +0300

    secont-task

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,13 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(a, b , c) {
+    if(a > b && a > c) {
+        return a
+    } else if( b > a && b > c) {
+        return b
+    } else {
+        return c
+    }
+}
+let resultR = findLargest(4, 8, 4)
+console.log(resultR)
