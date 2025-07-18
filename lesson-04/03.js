30e6d5ebb8d29a743e487927fd80baeedd4878d3
kicym <kicym.111@gmail.com>
2025-07-18 16:06:31 +0300

    three-task

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -4,7 +4,7 @@
 Входные данные:
 
 - `array1`: Первый массив, содержащий любые типы данных.
-- `array2`: Второй массив, содержащий любые типы данных.
+- `array1`: Второй массив, содержащий любые типы данных.
 
 Выходные данные:
 
@@ -17,4 +17,15 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(array1, array2) {
+    let newArray1 = []
+    for (let i = 0; i < array1.length; i++) {
+        if (includesElement(array2, array1[i])) {
+            newArray1.push(array1[i])
+
+        }
+
+    }
+
+    return newArray1
+}
