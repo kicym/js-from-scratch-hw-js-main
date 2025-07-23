2daa396c25301637567182766c1ab0ec035e6baf
kicym <kicym.111@gmail.com>
2025-07-23 22:58:20 +0300

    lesson_5_1

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,7 +15,11 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {
-
+  addResource(resource, amount) {
+    if (game.resources[resource] === undefined) {
+      console.log("Invalid resource")
+    } else {
+      game.resources[resource] += amount
+    }
   }
 }
