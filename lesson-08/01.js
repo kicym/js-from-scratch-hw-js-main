622dec68c63529bbfabecf9137353fa53951ac02
kicym <kicym.111@gmail.com>
2025-08-16 17:00:50 +0300

    task-81

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,18 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+     let text = str[0].toUpperCase()
+
+    for (let i = 1; i < str.length; i++) {
+
+        if(str[i - 1] === ' ' ) {
+            text = text + str[i].toUpperCase()
+            continue
+        }
+        text = text + str[i]
+    }
+     return text
+}
+
+
