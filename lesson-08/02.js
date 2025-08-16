22dca429f314ea59e5f8c081398f1def724daf5f
kicym <kicym.111@gmail.com>
2025-08-16 17:22:55 +0300

    task-82

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -5,10 +5,14 @@
 
 function isNumeric(str) {
   // your code
+    if (Number(str)) {
+        return true
+    }
+    return false
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
